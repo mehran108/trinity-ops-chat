be9f3a9e13c6239c99911d55ff2d18b64d854822
Muhammadkhizar <khizeraziz786@gmail.com>
2023-08-08 03:54:01 +0500

    work on home page and faq page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,17 @@ import { MatTabsModule } from '@angular/material/tabs';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { FaqComponent } from './faq/faq.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [			
+    AppComponent,
+      HomeComponent,
+      HeaderComponent,
+      FaqComponent
+   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
